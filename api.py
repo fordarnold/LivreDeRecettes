9252b16820e195b2fa98f44b12ab4a4c7fb88c8e
robin hood <fordarnold@gmail.com>
2015-03-29 20:37:42 +0300

    Added /cookbook resource. Errors still....

--- api.py
+++ api.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 from eve import Eve
+from eve.auth import BasicAuth
 
-# initialise Eve application
-app = Eve()
-
+class Authenticate(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        print resource
+        print method 
+        if resource == 'user' and method == 'GET':
+            user = app.data.driver.db['user']
+            user = user.find_one({'username': username,'password':password})
+            
+            if user:
+                return True
+            else:
+                return False
+        elif resource == 'user' and method == 'POST':
+            print username
+            print password
+            return username == 'admin' and password == 'password'
+        else:
+            return True
+            
 # run the app when the program is executed as a main program
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = Eve(auth=Authenticate)
+    app.run()
--- settings.py
+++ settings.py
@@ -1,1 +1,54 @@
-DOMAIN = {'user': {}}
+# Enable HTTP methods
+RESOURCE_METHODS = ['GET', 'POST']
+
+DOMAIN = {
+
+	'user': {
+		'additional_lookup': {
+        	'url': 'regex("[\w]+")',
+        	'field': 'username',
+        },
+		'schema': {
+            'first_name': {
+                'type': 'string'
+            },
+            'last_name': {
+                'type': 'string'
+            },
+            'email': {
+                'type': 'string',
+                'unique': True
+            },
+            'username': {
+                'type': 'string',
+                'unique': True
+            },
+            'password': {
+                'type': 'string'
+            },
+            'activated': {
+                'type': 'boolean',
+                'default': True
+            }
+        }
+	},
+
+	'cookbook': {
+		'schema': {
+			'name': {
+                'type': 'string'
+            },
+            'description': {
+                'type': 'text',
+                'default': ''
+            },
+            'author': {
+                'type': 'integer'
+            },
+            'photo': {
+                'type': 'string'
+                'default': ''
+            }
+		}
+	}
+}
